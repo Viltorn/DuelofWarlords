67c9d8242a9e51ce56153e7e2a8d43276db5d003
Viltorn <viltorn@gmail.com>
2023-10-24 19:45:45 +0300

    change port for socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'; // import connection function
 
-const socket = io('https://duelsofwarlords.onrender.com:8080'); // initialize websocket connection
+const socket = io('https://duelsofwarlords.onrender.com'); // initialize websocket connection
 
 export default socket;
